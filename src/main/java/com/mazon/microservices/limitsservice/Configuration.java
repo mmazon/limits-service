f283f7f02b92195055621758e1daa495ca60b78d
Moacir Mazon <moacir.mazon@gmail.com>
2018-07-25 19:05:58 -0300

    adicionando controler de limit services

---
+++ src/main/java/com/mazon/microservices/limitsservice/Configuration.java
@@ -0,0 +1,25 @@
+package com.mazon.microservices.limitsservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+	private int minimum;
+	private int maximum;
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+
+}
---
+++ src/main/java/com/mazon/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.mazon.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mazon.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration config;
+	
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfiguration(){
+		return new LimitConfiguration(config.getMaximum(), config.getMinimum());
+	}
+
+}
